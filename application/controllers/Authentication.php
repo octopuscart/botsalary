ee654a0d70a429add0c3ef8b9bcf23aa0f5f014c
octopuscart <dev@develop.com>
2021-11-17 02:16:45 +0530

    add files oct

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -7,6 +7,14 @@ class Authentication extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->userdata = $this->session->userdata('logged_in');
+        $session_user = $this->session->userdata('logged_in');
+        if ($session_user) {
+            $this->user_id = $session_user['login_id'];
+        } else {
+            $this->user_id = 0;
+        }
+        $this->user_id = $this->session->userdata('logged_in')['login_id'];
+        $this->user_type = $this->session->logged_in['user_type'];
     }
 
     public function index() {
